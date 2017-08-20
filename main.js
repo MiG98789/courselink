ee6b14059f53cc8c313fc6e8907c1ef5c030daa7
Gian Miguel Del Mundo <gianmiguel98@gmail.com>
2017-08-20 13:54:29 +0800

    Rating table now appears below course name

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
-function addRatings(course) {
-    var courses = document.getElementsByClassName("course");
+function addCourseRatings(ratings) {
+    var courseTables = document.getElementsByClassName("sections");
 
-    for (var i = 0; i < courses.length; i++) {
-        var rating = document.createElement("div");
-        rating.setAttribute("class", "rating");
-        rating.innerHTML = "<div class='rating'><table id='ratingTable'><tr id='ratingLabel'><td>Content</td><td>Teaching</td><td>Grading</td><td>Workload</td></tr><tr id='ratingRow'><td>" + course.content + "</td><td>" + course.teaching + "</td><td>" + course.grading + "</td><td>" + course.workload + "</td></tr></table></div>";
+    for (var i = 0; i < courseTables.length; i++) {
+        // var rating = document.createElement("div");
+        // rating.setAttribute("class", "course rating");
+        var div = [
+        "<div class='rating'><table id='ratingTable'><tr id='ratingTableHeader'>",
+        "<td>Content</td><td>Teaching</td><td>Grading</td><td>Workload</td>",
+        "</tr><tr id='ratingRow'>",
+        "<td>", ratings[0].content, "</td>",
+        "<td>", ratings[0].teaching, "</td>",
+        "<td>", ratings[0].grading, "</td>",
+        "<td>", ratings[0].workload, "</td>",
+        "</tr></table></div>"
+        ].join('');
 
         console.log("Rating course " + i);
-        courses[i].appendChild(rating);
+        courseTables[i].insertAdjacentHTML("beforebegin", div);
     }
 }
 
 if (document.readyState === "complete") {
-    console.log("Attempting to insert");
-    var test = {
+    var test = [
+    {
         "content":"A",
         "teaching":"A",
         "grading":"A",
         "workload":"A"
-    };
-    addRatings(test);
+    }
+    ];
+    addCourseRatings(test);
 }
