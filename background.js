69cd1d457954e2e471f84950243b42e994e4ad14
Gian Miguel Del Mundo <gmsdelmundo@gmail.com>
2017-08-20 16:28:53 +0800

    Stashing until can circumvent CORS

--- background.js
+++ background.js
@@ -1,3 +1,35 @@
+function sendRequest(url) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log("XHTTP URL: " + url);
+            console.log("XHTTP response: " + xhttp.responseUrl);
+
+            return response = xhttp.responseUrl;
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function scrapeCourse(courseCode) {
+    // Need to be logged into https://ust.space
+    var spaceURL = "https://ust.space/review/" + courseCode;
+    console.log("USTSPACE URL: " + spaceURL);
+
+    return sendRequest(spaceURL);
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log("Reached background.js message listener");
+        var response = scrapeCourse(request.code);
+        sendResponse(response);
+    }
+);
+
+//chrome.tabs.onUpdated.addListener(function() {});
+
 chrome.browserAction.onClicked.addListener(function() {
-        chrome.tabs.create({'url': "https://w5.ab.ust.hk/wcq/cgi-bin/1710/"});
-});
+    chrome.tabs.create({"url": "https://w5.ab.ust.hk/wcq/cgi-bin/1710/"});
+}); 
--- main.js
+++ main.js
@@ -1,25 +1,52 @@
+function getCourseRating(course) {
+    return chrome.runtime.sendMessage(course, function(response) {
+        console.log("Message response: " + response);
+    });
+}
+
 function addCourseRatings(ratings) {
     var courseTables = document.getElementsByClassName("sections");
-    var courses = document.getElementsByTagName("h2");
 
     for (var i = 0; i < courseTables.length; i++) {
-        var courseCode = courses[i].innerHTML.slice(0,10).replace(/\s/g, '');
         // var rating = document.createElement("div");
         // rating.setAttribute("class", "course rating");
         var div = [
-        "<div class='rating'><table id='ratingTable'><tr id='ratingTableHeader'>",
-        "<td>Content</td><td>Teaching</td><td>Grading</td><td>Workload</td>",
-        "</tr><tr id='ratingRow'>",
-        "<td>", ratings[0].content, "</td>",
-        "<td>", ratings[0].teaching, "</td>",
-        "<td>", ratings[0].grading, "</td>",
-        "<td>", ratings[0].workload, "</td>",
-        "</tr></table></div>"
+            "<div class='rating'><table id='ratingTable'><tr id='ratingTableHeader'>",
+            "<td>Content</td><td>Teaching</td><td>Grading</td><td>Workload</td>",
+            "</tr><tr id='ratingRow'>",
+            "<td>", ratings[0].content, "</td>",
+            "<td>", ratings[0].teaching, "</td>",
+            "<td>", ratings[0].grading, "</td>",
+            "<td>", ratings[0].workload, "</td>",
+            "</tr></table></div>"
         ].join('');
 
-        console.log("Rating " + courseCode);
         courseTables[i].insertAdjacentHTML("beforebegin", div);
     }
+}
+
+function addToPage() {
+    addCSS();
+
+    var courses = document.getElementsByTagName("h2");
+    var ratings = [];
+
+    for (var i = 0; i < courses.length; i++) {
+        var courseCode = courses[i].innerHTML.slice(0, 10).replace(/\s/g, '');
+        var courseJSON = {code: courseCode};
+        console.log("Getting " + courseJSON.code);
+        getCourseRating(courseJSON);
+    }
+
+    var test = [
+        {
+            "content": "A",
+            "teaching": "A",
+            "grading": "A",
+            "workload": "A"
+        }
+    ];
+    addCourseRatings(test);
 }
 
 function addCSS() {
@@ -38,15 +65,5 @@ function addCSS() {
 }
 
 if (document.readyState === "complete") {
-    addCSS();
-    
-    var test = [
-    {
-        "content":"A",
-        "teaching":"A",
-        "grading":"A",
-        "workload":"A"
-    }
-    ];
-    addCourseRatings(test);
+    addToPage();
 }
