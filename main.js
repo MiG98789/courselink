c31b5b5132a2528722f419222e338e1c055ecad6
Gian Miguel Del Mundo <gmsdelmundo@gmail.com>
2017-08-20 19:29:19 +0800

    Updated description, and added hyperlink styling

--- main.js
+++ main.js
@@ -1,28 +1,13 @@
-function addURLs() {
+if (document.readyState === "complete") {
     var courses = document.getElementsByTagName("h2");
 
+    console.log("Generating URLs");
     for (var i = 0; i < courses.length; i++) {
         var courseCode = courses[i].innerHTML.slice(0,10).replace(/\s/g, '');
-        console.log("Found " + courseCode);
 
         var url = "https://ust.space/review/" + courseCode;
+        console.log(url);
 
-        courses[i].innerHTML += " <a target='_blank' href=" + url + ">Rating</a>";
+        courses[i].innerHTML +=  " <a class='rating up down' target='_blank' href=" + url + ">Rating</a>";
     }
-}
-
-function addCSS() {
-    var css = [
-    ].join('');
-
-    var style = document.createElement("style");
-    style.setAttribute("type", "text/css");
-    style.appendChild(document.createTextNode(css));
-
-    document.head.appendChild(style);
-}
-
-if (document.readyState === "complete") {
-    addCSS();
-    addURLs();
 }
