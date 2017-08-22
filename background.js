8827efb020b2aed6a64b25d018657d8a783d288a
Gian Miguel Del Mundo <gmsdelmundo@gmail.com>
2017-08-22 22:54:12 +0800

    Fixed CORS issue: can now retrieve USTSPACE webpages

--- background.js
+++ background.js
@@ -1,3 +1,32 @@
+function sendRequest(url) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log("XHTTP URL: " + url);
+            console.log("XHTTP response: " + xhttp.responseText);
+            return response = xhttp.responseUrl;
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function scrapeCourse(courseCode) {
+    // Need to be logged into https://ust.space
+    // TODO: Open tab to log into USTSPACE if not logged in
+    var spaceURL = "https://ust.space/review/" + courseCode;
+    console.log("USTSPACE URL: " + spaceURL);
+    return sendRequest(spaceURL);
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log("Received message from " + request.code);
+        var response = scrapeCourse(request.code);
+        sendResponse(response);
+    }
+);
+
 chrome.browserAction.onClicked.addListener(function() {
         chrome.tabs.create({'url': "https://w5.ab.ust.hk/wcq/cgi-bin/1710/"});
 });
--- main.js
+++ main.js
@@ -1,13 +1,20 @@
+function getCoursePage(course) {
+    return chrome.runtime.sendMessage(course, function(response) {
+        console.log("Message response: " + response);
+    });
+}
+
 if (document.readyState === "complete") {
     var courses = document.getElementsByTagName("h2");
 
-    console.log("Generating URLs");
     for (var i = 0; i < courses.length; i++) {
-        var courseCode = courses[i].innerHTML.slice(0,10).replace(/\s/g, '');
-
+        var courseCode = courses[i].innerText.slice(0,10).replace(/\s/g, '');
         var url = "https://ust.space/review/" + courseCode;
-        console.log(url);
-
+        console.log(courseCode + "\'s URL: " + url);
         courses[i].innerHTML +=  " <a class='rating lower-bar' target='_blank' href=" + url + ">Rating</a>";
+
+        var courseJSON = { code: courseCode };
+        console.log("Getting " + courseJSON.code + " rating");
+        getCoursePage(courseJSON);
     }
 }
